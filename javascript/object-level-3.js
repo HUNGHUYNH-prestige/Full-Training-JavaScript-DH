cd16b064f6c71628a33f46c097faf6d22a84337b
Hung <hunghuynh22@gmail.com>
2022-01-24 19:40:21 +0100

    phase 4 : correction algo

--- javascript/object-level-3.js
+++ javascript/object-level-3.js
@@ -34,84 +34,36 @@ console.log(recordCollection);
 
 function updateRecords(records, id, prop, value) {
 
+    let item = records[id];
+
     // 1. if the prop isn't tracks : done with hasOwnProperty()
     // 2. value is not an empty string
-    if (records[id].hasOwnProperty(prop) != "tracks" && value != "") {
-        //console.log(prop);
-        //console.log(value);
-        records[id][prop] = value;
-        let new_prop = value;
-        console.log(new_prop)
-        //console.log(records);
+    if (prop != "tracks" && value != "") {
+        item[prop] = value;
     }
 
     // 3. if props is tracks : checked
     // 4. if tracks is not in the object property : create an empty array and add the value to it
-    if (records[id].hasOwnProperty("tracks")){
-        console.log(records[id].hasOwnProperty("tracks") + " ok");
-        console.log("this is ok");
-        
-
-    
-    }
-    else {
-        console.log("SORRY NO TRACKS !");
-        // before creating the array and adding the value to it
-        // the method hasOwnProperty becomes : false
-        console.log(records[id].hasOwnProperty("tracks"));
-    
-        console.log(value);
-
-        // Creating the ARRAY
-        records[id].tracks = [];
-        // Adding the value to it
-        records[id].tracks[0] = value;
-
-        console.log(records[id].tracks);
-        console.log(records);
-        // after creating the array and adding the value to it
-        // the method hasOwnProperty becomes : true
-        console.log(records[id].hasOwnProperty("tracks"));
+    if (prop == " tracks" && !item.hasOwnProperty("tracks")) {
+        item[prop] = [];
+        item[prop].push(value);
     }
 
 
     // 5. if props is tracks : checked
     // 6. if value isn't an empty string : add the value to the end of the existing tracks array
-    if (records[id].hasOwnProperty("tracks") && value != ""){
-        //console.log("this is ok");
-        //console.log("value not empty : " + value);
-        //console.log(records[id].tracks.length -1);
-        let array_length = records[id].tracks.length;
-        //console.log(array_length);
-        records[id].tracks[array_length] = value;
-        console.log(records[id].tracks); 
-
+    if (prop == "tracks" && value != "") {
+        item[prop].push(value);
     }
 
     // 7. if value is an empty string : delete the given prop
-    if (value == ''){
-        const text_string_is_empty = "value is an empty string ==> nothing inside this value";
-        console.log(text_string_is_empty);
-        console.log(records[id].hasOwnProperty(prop));
-        console.log(records)
-
-        if (!records[id].hasOwnProperty(prop)){
-            console.log(records[id].hasOwnProperty(prop));
-            console.log(prop);
-        }
-        else {
-            console.log(records[id].hasOwnProperty(prop));
-            delete records[id][prop];
-        }
-    }
-    else {
-        const text_is_not_empty = "there is something inside this value, the value is : " + value;
-        console.log(text_is_not_empty);
+    if (value != "") {
+        delete item[prop];
     }
 
     // always return the full object
-    //return records;
-    return "return the full object at the end";
+    return records;
+    //return "return the full object at the end";
 }
 
 
@@ -121,11 +73,11 @@ let result_case1 = updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 console.log(result_case1);
 
 
-/*
+
 // Test normal condition : case 2
 let result_case2 = updateRecords(recordCollection, 5439, 'artist', 'test');
 console.log(result_case2);
-*/
+
 
 
 // Test string empty : nothing inside
@@ -134,7 +86,7 @@ let result_empty_string = updateRecords(recordCollection, 5439, 'artist', empty)
 console.log(result_empty_string);
 
 
-/*
+
 // testing the limit of console.log in the console for fun
 let pomme = 'pomme';
 console.log(pomme);
@@ -146,4 +98,3 @@ console.log(pomme, banane, poire);
 // END OF OBJECT LEVEL 3
 console.log("END OF OBJECT LEVEL 3");
 console.log('>>> >>> >>> >>> >>>');
-*/

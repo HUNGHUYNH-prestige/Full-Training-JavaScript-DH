ead152bbe42c49be1d25716085b3c4be659526b6
Hung <hunghuynh22@gmail.com>
2022-02-04 17:17:31 +0100

    for loop and recursion

--- javascript/object-level-9.js
+++ javascript/object-level-9.js
@@ -12,17 +12,25 @@ function range (startNum, endNum) {
     let array = [];
 
     for (let ok = startNum; ok <= endNum; ok++) {
+        if (ok < 0 || endNum < 0) {
+            console.log(array);
+            return array;
+            
+        }
         array.push(ok);
+        //console.log(ok);
+        //array.concat(ok);
     }
-
     console.log(array);
 };
   
-  
+ // Correct cases 
 range(1, 5);
 range(6,9);
 range(4,4);
 range(0,0);
+
+// Incorrect cases
 range(-1,0);
 range(-10,-1000);
 range(0, -1);
@@ -33,3 +41,126 @@ range(0, -1);
 
 
 
+
+
+
+
+
+
+
+
+
+let array_something = [1,2,3];
+console.log(array_something);
+
+let concat_something = [10, 20, 30];
+array_something.concat(concat_something);
+
+console.log(concat_something);
+
+console.log(array_something);
+
+// Nohting is happening ?! 
+//       eh...!!!           O_o  _______oh!
+
+// When use array.concat() method
+// Any array is changed
+// Everything is stable and nothing changes
+
+
+let array_1 = [11,22,33];
+console.log(array_1);
+
+let concat_new_array = [99, 88, 77];
+console.log(concat_new_array);
+// This is the result of concatenating the new array with the original array
+let fusion = array_1.concat(concat_new_array);
+console.log("This is the fusion array with the concat method :");
+console.log(fusion);
+
+
+// This is unchanged array
+console.log("Unchanged array :");
+console.log(array_1);
+// This is unchanged concat new array
+console.log("Unchanged concat new array :");
+console.log(concat_new_array);
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+// Let's play with concat
+console.log(`--- --- --- --- ---`);
+console.log(`It's time to concat`);
+console.log(`--- --- --- --- ---`);
+
+
+function concatenate_something(start, end) {
+
+    let result_response = [];
+
+    // Incorrect cases and situations
+    if (start > end) {
+        //console.log("Attention : \nstart ==> { " + start + " } <== \nis bigger \nthan end ==> { " + end + " } <==");
+        return result_response;
+    }
+    else if (start < 0 ) {
+        //console.log("Attention : start is negative " + "\nThe value is : " + start);
+        return result_response;
+    }
+    else if (end < 0) {
+        //console.log(`Attention ! The value of end is ${end}`);
+        return result_response;
+    }
+
+    // Correct cases and situations
+    else if (start == end) {
+        console.log(`The start is ${start} \nand \nthe end is ${end}`);
+        return result_response = [start];
+    }
+    else {
+        //console.log(`Here is correct !`);
+        console.log(`The start is ${start} \nand the end is ${end}`);
+        result_response = result_response.concat(concatenate_something(start + 1, end));
+        return result_response.concat(start);
+
+    }
+}
+
+
+console.log(concatenate_something(1,5));
+console.log(concatenate_something(0,0));
+console.log(concatenate_something(-1,0));
+console.log(concatenate_something(-1,-2));
+console.log(concatenate_something(10,1));
+
+/*
+let content = concatenate_something(2,5);
+console.log(content);
+*/
+
+
+
+
+// == == == == == == == == == == == == == == == == ==
+
+/*
+
+const omega = ["o", "m", "e", "g", "a"];
+console.log(omega);
+
+const omega_concat = ["c", "o", "n", "c", "a", "t"];
+console.log(omega_concat);
+
+const final_omega_concat = omega.concat("- space -", omega_concat, "something else");
+console.log(final_omega_concat);
+
+*/
+
+// == == == == == == == == == == == == == == == == ==
